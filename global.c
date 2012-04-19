35faba1fd0b3dfab2f081bf96a5eb695b73dae9d
Pallamidessi joseph <sangdragon777@gmail.com>
2012-04-19 12:18:21 +0200

    contient la fonction du menu entre autre

---
+++ global.c
@@ -0,0 +1,237 @@
+#include"global.h"
+
+
+int menu(){
+	int entree=0;
+	int posMenu=5;
+	
+	while(entree!=TOUCHE_ECHAP){
+	ChangerCouleurFond(37);
+	/*I*/
+	SePositionner(10,10);
+	printf(" ");
+	SePositionner(11,10);
+	printf(" ");
+	SePositionner(12,10);
+	printf(" ");
+	SePositionner(11,11);
+	printf(" ");
+	SePositionner(11,11);
+	printf(" ");
+	SePositionner(10,12);
+	printf(" ");
+	SePositionner(11,12);
+	printf(" ");
+	SePositionner(12,12);
+	printf(" ");
+	/*N*/
+	SePositionner(14,10);
+	printf(" ");
+	SePositionner(17,10);
+	printf(" ");
+	SePositionner(14,11);
+	printf(" ");
+	SePositionner(14,12);
+	printf(" ");
+	SePositionner(15,11);
+	printf(" ");
+	SePositionner(16,12);
+	printf(" ");
+	SePositionner(17,11);
+	printf(" ");
+	SePositionner(17,12);
+	printf(" ");
+	/*F*/
+	SePositionner(19,10);
+	printf(" ");
+	SePositionner(20,10);
+	printf(" ");
+	SePositionner(21,10);
+	printf(" ");
+	SePositionner(19,11);
+	printf(" ");
+	SePositionner(20,11);
+	printf(" ");
+	SePositionner(19,12);
+	printf(" ");
+	/*i*/
+	SePositionner(23,10);
+	printf(" ");
+	SePositionner(23,11);
+	printf(" ");
+	SePositionner(23,12);
+	printf(" ");
+
+	/*N*/
+	SePositionner(25,10);
+	printf(" ");
+	SePositionner(28,10);
+	printf(" ");
+	SePositionner(25,11);
+	printf(" ");
+	SePositionner(25,12);
+	printf(" ");
+	SePositionner(26,11);
+	printf(" ");
+	SePositionner(27,12);
+	printf(" ");
+	SePositionner(28,11);
+	printf(" ");
+	SePositionner(28,12);
+	printf(" ");
+
+	/*i*/
+	SePositionner(30,10);
+	printf(" ");
+	SePositionner(30,11);
+	printf(" ");
+	SePositionner(30,12);
+	printf(" ");
+
+	/*T*/
+
+	SePositionner(32,10);
+	printf(" ");
+	SePositionner(33,10);
+	printf(" ");
+	SePositionner(34,10);
+	printf(" ");
+	SePositionner(33,11);
+	printf(" ");
+	SePositionner(33,12);
+	printf(" ");
+	/*E*/
+	SePositionner(35,10);
+	printf(" ");
+	SePositionner(36,10);
+	printf(" ");
+	SePositionner(37,10);
+	printf(" ");
+	SePositionner(35,11);
+	printf(" ");
+	SePositionner(36,11);
+	printf(" ");
+	SePositionner(35,12);
+	printf(" ");
+	SePositionner(36,12);
+	printf(" ");
+	SePositionner(37,12);
+	printf(" ");
+	/*A la ligne*/
+	/*Q*/
+	SePositionner(20,15);
+	printf(" ");
+	SePositionner(21,14);
+	printf(" ");
+	SePositionner(21,16);
+	printf(" ");
+	SePositionner(22,15);
+	printf(" ");
+	SePositionner(22,16);
+	printf(" ");
+	/*U*/
+	SePositionner(24,14);
+	printf(" ");
+	SePositionner(24,15);
+	printf(" ");
+	SePositionner(24,16);
+	printf(" ");
+	SePositionner(25,16);
+	printf(" ");
+	SePositionner(26,14);
+	printf(" ");
+	SePositionner(26,15);
+	printf(" ");
+	SePositionner(26,16);
+	printf(" ");
+	/*E*/
+	SePositionner(28,14);
+	printf(" ");
+	SePositionner(29,14);
+	printf(" ");
+	SePositionner(30,14);
+	printf(" ");
+	SePositionner(28,15);
+	printf(" ");
+	SePositionner(29,15);
+	printf(" ");
+	SePositionner(28,16);
+	printf(" ");
+	SePositionner(29,16);
+	printf(" ");
+	SePositionner(30,16);
+	printf(" ");
+		
+	/*S*/
+	SePositionner(32,15);
+	printf(" ");
+	SePositionner(32,17);
+	printf(" ");
+	SePositionner(33,14);
+	printf(" ");
+	SePositionner(33,16);
+	printf(" ");
+	/*T*/
+	SePositionner(35,14);
+	printf(" ");
+	SePositionner(36,14);
+	printf(" ");
+	SePositionner(37,14);
+	printf(" ");
+	SePositionner(36,15);
+	printf(" ");
+	SePositionner(36,16);
+	printf(" ");
+	
+	ReinitialiserCouleur();
+if(posMenu==5){
+		SePositionner(15,20);
+		printf("Continuer");
+		SePositionner(15,21);
+		printf("Quitter");
+		posMenu=0;
+		}
+
+	entree=RecupererAppuiTouche();
+
+
+	if(entree==TOUCHE_FLECHE_BAS){
+		if(posMenu<NBR_MENU)
+			posMenu++;
+	}
+	else 
+		if(entree==TOUCHE_FLECHE_HAUT){
+			if(posMenu>0)
+				posMenu--;
+		}else
+			if(entree==TOUCHE_ENTREE){
+				return posMenu;
+				}
+
+	if(posMenu==0){
+		ChangerCouleurFond(33);
+		ChangerCouleurTexte(30);
+		SePositionner(15,20);
+		printf("Continuer");
+		ReinitialiserCouleur();
+
+		SePositionner(15,21);
+		printf("Quitter");
+	}
+	else 
+		if(posMenu==1){
+			SePositionner(15,20);
+			printf("Continuer");
+
+			ChangerCouleurFond(33);
+			ChangerCouleurTexte(30);
+			SePositionner(15,21);
+			printf("Quitter");
+			ReinitialiserCouleur();
+			}
+	}	
+
+	EffacerEcran();
+return 0;
+	
+	}
